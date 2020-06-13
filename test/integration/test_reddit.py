4e5baef24f62d5dbf566eaa8e860d0cc0a157bfa
Dan6erbond <moravrav@gmail.com>
2020-06-13 07:45:15 +0200

    Rename private variables in tests
    Rename private variable `reddit` to `_reddit` in test cases and using `asyncSetUp` to store reused variables.

--- test/integration/test_reddit.py
+++ test/integration/test_reddit.py
@@ -7,22 +7,22 @@ class RedditTest(unittest.IsolatedAsyncioTestCase):
     def __init__(self, *args, **kwargs):
         super(RedditTest, self).__init__(*args, **kwargs)
 
-        self.reddit = apraw.Reddit("APB")
+        self._reddit = apraw.Reddit("APB")
 
     async def test_reddit_subreddit(self):
-        subreddit = await self.reddit.subreddit("aPRAWTest")
+        subreddit = await self._reddit.subreddit("aPRAWTest")
         self.assertEqual(subreddit.description, "Testing subreddit for aPRAW.")
 
     async def test_reddit_submission(self):
-        submission = await self.reddit.submission("h7mna9")
+        submission = await self._reddit.submission("h7mna9")
         self.assertEqual(submission.title, "Test Post")
 
     async def test_reddit_comment(self):
-        comment = await self.reddit.comment("fulsybg")
+        comment = await self._reddit.comment("fulsybg")
         self.assertEqual(comment.body, "This is a test comment.")
 
     async def test_reddit_redditor(self):
-        redditor = await self.reddit.redditor("Dan6erbond")
+        redditor = await self._reddit.redditor("Dan6erbond")
         self.assertEqual(redditor.id, "11qzch")
 
 
