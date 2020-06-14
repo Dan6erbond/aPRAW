4cd8a88d14b3a75f42de1438cbb966dac0549d1f
Dan6erbond <moravrav@gmail.com>
2020-06-14 18:54:06 +0200

    Add type-hinting to TestReddit for better tooling

--- tests/integration/test_reddit.py
+++ tests/integration/test_reddit.py
@@ -1,23 +1,25 @@
 import pytest
 
+import apraw
+
 
 class TestReddit:
     @pytest.mark.asyncio
-    async def test_reddit_subreddit(self, reddit):
+    async def test_reddit_subreddit(self, reddit: apraw.Reddit):
         subreddit = await reddit.subreddit("aPRAWTest")
         assert subreddit.description == "Testing subreddit for aPRAW."
 
     @pytest.mark.asyncio
-    async def test_reddit_submission(self, reddit):
+    async def test_reddit_submission(self, reddit: apraw.Reddit):
         submission = await reddit.submission("h7mna9")
         assert submission.title == "Test Post"
 
     @pytest.mark.asyncio
-    async def test_reddit_comment(self, reddit):
+    async def test_reddit_comment(self, reddit: apraw.Reddit):
         comment = await reddit.comment("fulsybg")
         assert comment.body == "This is a test comment."
 
     @pytest.mark.asyncio
-    async def test_reddit_redditor(self, reddit):
+    async def test_reddit_redditor(self, reddit: apraw.Reddit):
         redditor = await reddit.redditor("Dan6erbond")
         assert redditor.id == "11qzch"
