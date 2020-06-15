4c70782697ffd45bc22d6a99c9b9ffa18eb34498
Dan6erbond <moravrav@gmail.com>
2020-06-15 09:13:39 +0200

    Add subreddit method
    Add subreddit method and dependency injection for when subreddit is already known.

--- apraw/models/comment.py
+++ apraw/models/comment.py
@@ -5,12 +5,13 @@ from ..endpoints import API_PATH
 
 class Comment:
 
-    def __init__(self, reddit, data, submission=None, author=None):
+    def __init__(self, reddit, data, submission=None, author=None, subreddit=None):
         self.reddit = reddit
         self.data = data
 
         self._submission = submission
         self._author = author
+        self._subreddit = subreddit
 
         self.id = data["id"]
         self.created_utc = datetime.utcfromtimestamp(data["created_utc"])
@@ -19,6 +20,8 @@ class Comment:
         self.archived = data["archived"]
         self.link_id = data["link_id"]
         self.parent_id = data["parent_id"]
+        self.subreddit_name = data["subreddit"]
+        self.subreddit_id = data["subreddit_id"]
         self.score = data["score"]
         self.body = data["body"]
         self.is_submitter = data["is_submitter"]
@@ -39,3 +42,8 @@ class Comment:
             self._submission = Submission(
                 self.reddit, link["data"]["children"][0]["data"])
         return self._submission
+
+    async def subreddit(self):
+        if self._subreddit is None:
+            self._subreddit = await self.reddit.subreddit(self.subreddit_name)
+        return self._subreddit
