5dccbdd87ca5e12db6009b799df6798b3519211c
Dan6erbond <moravrav@gmail.com>
2020-06-18 14:42:02 +0200

    Implement comment.reply()
     - Add privates `_replies` and `_full_data` to comment.
     - Add `full_data()` and `refresh()` to comment.
     - Add public `replies()` async generator that yields a comment's replies and nests subcomments.
     - Add endpoint to grab comment full data.

--- apraw/endpoints.py
+++ apraw/endpoints.py
@@ -4,16 +4,16 @@
 BASE_URL = "https://oauth.reddit.com{}?{}"
 
 API_PATH = {
+    "comment"              : "/r/{sub}/comments/{submission}/_/{id}",
     "compose"              : "/api/compose",
     "info"                 : "/api/info",
-    "me"                   : "/api/v1/me",
     "me_karma"             : "/api/v1/me/karma",
+    "me"                   : "/api/v1/me",
     "moderated"            : "/user/{user}/moderated_subreddits",
     "modmail_conversation" : "/api/mod/conversations/{id}",
     "modmail_conversations": "/api/mod/conversations",
     "morechildren"         : "/api/morechildren",
     "submission"           : "/r/{sub}/comments/{id}",
-    "subreddit"            : "/r/{sub}",
     "subreddit_about"      : "/r/{sub}/about",
     "subreddit_comments"   : "/r/{sub}/comments",
     "subreddit_edited"     : "/r/{sub}/about/edited",
@@ -27,6 +27,7 @@ API_PATH = {
     "subreddit_spam"       : "/r/{sub}/about/spam",
     "subreddit_top"        : "/r/{sub}/top",
     "subreddit_unmoderated": "/r/{sub}/about/unmoderated",
+    "subreddit"            : "/r/{sub}",
     "subreddits_new"       : "/subreddits/new",
     "user_about"           : "/user/{user}/about",
     "user_comments"        : "/user/{user}/comments",
--- apraw/models/comment.py
+++ apraw/models/comment.py
@@ -6,12 +6,14 @@ from .apraw_base import aPRAWBase
 class Comment(aPRAWBase):
 
     def __init__(self, reddit, data, submission=None,
-                 author=None, subreddit=None):
+                 author=None, subreddit=None, replies=None):
         super().__init__(reddit, data)
 
         self._submission = submission
         self._author = author
         self._subreddit = subreddit
+        self._full_data = None
+        self._replies = replies
 
         self.subreddit_name = data["subreddit"]
         self.url = "https://www.reddit.com" + data["permalink"]
@@ -33,3 +35,44 @@ class Comment(aPRAWBase):
         if self._subreddit is None:
             self._subreddit = await self.reddit.subreddit(self.subreddit_name)
         return self._subreddit
+
+    async def full_data(self, refresh=False):
+        if self._full_data is None or refresh:
+            self._full_data = await self.reddit.get_request(
+                API_PATH["comment"].format(sub=self.data["subreddit"],
+                                           submission=self.data["link_id"].replace(
+                                               self.reddit.link_kind + "_", ""),
+                                           id=self.data["id"]))
+        return self._full_data
+
+    async def refresh(self):
+        await self.full_data(True)
+        await self.replies(True)
+
+    async def replies(self, refresh=False):
+        if self._replies is None or refresh:
+            fd = await self.full_data()
+
+            def find(listing):
+                data = listing["data"]
+
+                for comment in data["children"]:
+                    if comment["data"]["id"] == self.id:
+                        return comment
+                    else:
+                        return find(comment["replies"])
+
+            comment = find(fd[1])
+
+            def get_replies(comment, replies=[]):
+                if "kind" in comment["replies"] and comment["replies"]["kind"] == self.reddit.listing_kind:
+                    for reply in comment["replies"]["data"]["children"]:
+                        data = reply["data"]
+                        replies.append(
+                            Comment(self.reddit, data, replies=get_replies(data, [])))
+                return replies
+
+            self._replies = get_replies(comment["data"])
+
+        for reply in self._replies:
+            yield reply
