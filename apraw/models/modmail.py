f654c1df20c3f769990ea0f698f59c41d6307486
Dan6erbond <moravrav@gmail.com>
2020-06-19 09:47:12 +0200

    Add type-hinting to SubredditModmail, ModmailConversation and ModmailMessage
    Using dynamic imports and forward-references to import classes otherwise resulting in cyclic imports.

--- apraw/models/modmail.py
+++ apraw/models/modmail.py
@@ -1,20 +1,28 @@
+from typing import TYPE_CHECKING, Dict
+
 from ..endpoints import API_PATH
 from ..utils import snake_case_keys
 from .apraw_base import aPRAWBase
 
+if TYPE_CHECKING:
+    from .subreddit import Subreddit
+    from .redditor import Redditor
+    from ..reddit import Reddit
+
 
 class SubredditModmail:
-    def __init__(self, subreddit):
+    def __init__(self, subreddit: 'Subreddit'):
         self.subreddit = subreddit
 
-    async def conversations(self):
+    async def conversations(self) -> 'ModmailConversation':
         req = await self.subreddit.reddit.get_request(API_PATH["modmail_conversations"], entity=self.subreddit.display_name)
         for id in req["conversations"]:
             yield ModmailConversation(self.subreddit.reddit, req["conversations"][id])
 
 
 class ModmailConversation(aPRAWBase):
-    def __init__(self, reddit, data, owner=None):
+    def __init__(self, reddit: 'Reddit', data: Dict,
+                 owner: 'Subreddit' = None):
         super().__init__(reddit, data)
 
         self._data = None
@@ -22,24 +30,24 @@ class ModmailConversation(aPRAWBase):
         self.id = data["id"]
         self._owner = owner
 
-    async def owner(self):
+    async def owner(self) -> 'Subreddit':
         if self._owner is None:
             self._owner = await self.reddit.subreddit(self.data["owner"]["displayName"])
         return self._owner
 
-    async def messages(self):
+    async def messages(self) -> 'ModmailMessage':
         full_data = await self.full_data()
         for msg_id in full_data["messages"]:
             yield ModmailMessage(self, full_data["messages"][msg_id])
 
-    async def full_data(self):
+    async def full_data(self) -> Dict:
         if self._data is None:
             self._data = await self.reddit.get_request(API_PATH["modmail_conversation"].format(id=self.id))
         return self._data
 
 
 class ModmailMessage:
-    def __init__(self, conversation, data):
+    def __init__(self, conversation: ModmailConversation, data: Dict):
         self.conversation = conversation
         self.data = data
 
@@ -51,7 +59,7 @@ class ModmailMessage:
         self.is_internal = data["isInternal"]
         self.date = data["date"]
 
-    async def author(self):
+    async def author(self) -> 'Redditor':
         if self._author is None:
             if not self.data["author"]["isDeleted"]:
                 self._author = self.conversation.reddit.redditor(
