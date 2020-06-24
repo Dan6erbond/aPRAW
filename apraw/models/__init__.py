e803298060053d05ad8bc28175296456776847ce
Dan6erbond <moravrav@gmail.com>
2020-06-24 16:11:57 +0200

    Export classes
    Export classes for use in documentation.

--- apraw/models/__init__.py
+++ apraw/models/__init__.py
@@ -1,7 +1,8 @@
 from .comment import Comment
 from .redditor import Redditor
 from .submission import Submission
-from .subreddit import Subreddit
+from .subreddit import Subreddit, SubredditModerator, SubredditModmail, SubredditModeration
 from .listing_generator import ListingGenerator
 from .user import User
 from .subreddit_wiki import WikipageRevision
+from .apraw_base import aPRAWBase
