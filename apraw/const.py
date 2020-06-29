0a64fe645f1cc528988f1c4f3c6fd73df5096722
Dan6erbond <moravrav@gmail.com>
2020-06-29 19:47:58 +0200

    Version bump to 0.4.0

--- apraw/const.py
+++ apraw/const.py
@@ -1,7 +1,7 @@
 """aPRAW constants."""
 from .endpoints import API_PATH
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 __tag__ = "alpha"
 
 USER_AGENT_FORMAT = "{} aPRAW v" + __version__ + "-" + __tag__ + "by Dan6erbond"
