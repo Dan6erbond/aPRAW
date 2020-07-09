e8f8cbeb1fea305d1543420004f6931e911f0d83
Dan6erbond <moravrav@gmail.com>
2020-07-09 10:47:07 +0200

    Write tests
    Write tests for `CommentForrest.replace_more()` by ensuring all `class MoreChildren` are replaced.

--- apraw/models/__init__.py
+++ apraw/models/__init__.py
@@ -5,6 +5,7 @@ from .helpers.streamable import Streamable
 from .reddit.comment import Comment
 from .reddit.listing import Listing
 from .reddit.message import Message
+from .reddit.more_comments import MoreComments
 from .reddit.redditor import Redditor
 from .reddit.submission import Submission
 from .subreddit.moderation import ModAction
