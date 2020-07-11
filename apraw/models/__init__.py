b083f2f92034cf727149912cbf13003b209eca87
Dan6erbond <moravrav@gmail.com>
2020-07-11 18:29:21 +0200

    Closes #87
    Update `@streamable` decorator with `class ProxyStreamable` for bound methods and `class Streamable` for functions.

--- apraw/models/__init__.py
+++ apraw/models/__init__.py
@@ -1,7 +1,7 @@
 from .helpers.apraw_base import aPRAWBase
 from .helpers.generator import ListingGenerator
 from .helpers.item_moderation import ItemModeration, PostModeration
-from .helpers.streamable import Streamable
+from .helpers.streamable import Streamable, streamable
 from .reddit.comment import Comment
 from .reddit.listing import Listing
 from .reddit.message import Message
