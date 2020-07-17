588a9906bae2ccf872173899824b69728308d4f1
Dan6erbond <moravrav@gmail.com>
2020-07-17 17:01:10 +0200

    Add delete and update to RemovalReason

--- apraw/models/subreddit/removal_reasons.py
+++ apraw/models/subreddit/removal_reasons.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING, Dict, Optional
 
 from ..helpers.apraw_base import aPRAWBase
 from ...const import API_PATH
@@ -10,22 +10,37 @@ if TYPE_CHECKING:
 
 class SubredditRemovalReason(aPRAWBase):
 
-    def __init__(self, reddit: 'Reddit', data: Dict):
+    def __init__(self, reddit: 'Reddit', subreddit: 'Subreddit', data: Dict):
+        self._subreddit = subreddit
         super().__init__(reddit, data)
+
+    async def delete(self):
+        url = API_PATH["subreddit_removal_reason"].format(sub=self._subreddit.display_name, id=self.id)
+        res = await self._reddit.delete(url)
+        return res
+
+    async def update(self, message: Optional[str] = None, title: Optional[str] = None):
+        url = API_PATH["subreddit_removal_reason"].format(sub=self._subreddit.display_name, id=self.id)
+        data = {
+            k: v if v else getattr(self, k)
+            for k, v in {"message": message, "title": title}.items()
+        }
+        await self._reddit.put(url, data=data)
 
 
 class SubredditRemovalReasons:
 
-    def __init__(self, subreddit: 'Subreddit'):
+    def __init__(self, reddit: 'Reddit', subreddit: 'Subreddit'):
+        self._reddit = reddit
         self._subreddit = subreddit
         self._removal_reasons = list()
 
     async def _fetch(self):
-        res = await self._subreddit._reddit.get(
-            API_PATH["subreddit_removal_reasons"].format(sub=self._subreddit.display_name))
+        url = API_PATH["subreddit_removal_reasons"].format(sub=self._subreddit.display_name)
+        res = await self._reddit.get(url)
 
         for reason_id in res["order"]:
-            self._removal_reasons.append(SubredditRemovalReason(self._subreddit._reddit, res["data"][reason_id]))
+            self._removal_reasons.append(SubredditRemovalReason(self._reddit, res["data"][reason_id]))
 
     async def get(self, item: str):
         if not self._removal_reasons:
