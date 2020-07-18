134d6a8d6b6347d5a770969ee0fa025a6e65d97a
Dan6erbond <moravrav@gmail.com>
2020-07-18 14:21:52 +0200

    Add Reddit class docs

--- apraw/__init__.py
+++ apraw/__init__.py
@@ -1,2 +1,3 @@
 from .const import __version__, __tag__
 from .reddit import Reddit
+from .request_handler import RequestHandler
--- apraw/models/__init__.py
+++ apraw/models/__init__.py
@@ -2,15 +2,15 @@ from .helpers.apraw_base import aPRAWBase
 from .helpers.generator import ListingGenerator
 from .helpers.item_moderation import ItemModeration, PostModeration
 from .helpers.streamable import Streamable, streamable
-from .reddit.comment import Comment
+from .reddit.comment import Comment, CommentModeration
 from .reddit.listing import Listing
 from .reddit.message import Message
 from .reddit.more_comments import MoreComments
 from .reddit.redditor import Redditor
-from .reddit.submission import Submission
-from .subreddit.moderation import ModAction
+from .reddit.submission import Submission, SubmissionModeration
+from .subreddit.moderation import ModAction, SubredditModerator, SubredditModeration
 from .subreddit.modmail import ModmailConversation, ModmailMessage, SubredditModmail
 from .subreddit.removal_reasons import SubredditRemovalReason, SubredditRemovalReasons
 from .subreddit.subreddit import Subreddit
-from .subreddit.wiki import WikipageRevision
+from .subreddit.wiki import WikipageRevision, SubredditWiki, SubredditWikipage
 from .user import AuthenticatedUser, Karma, User
